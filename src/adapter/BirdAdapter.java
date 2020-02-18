b45d67bd6612fd22b1eb4cc7577eb55d5fde72d4
Sergii.Pankrashyn <Sergii.Pankrashyn.9@fntsoftware.com>
2020-02-18 15:01:26 +0200

    Add Adapter pattern

---
+++ src/adapter/BirdAdapter.java
@@ -0,0 +1,14 @@
+package adapter;
+
+public class BirdAdapter implements ToyDuck {
+    Bird bird;
+
+    public BirdAdapter(Bird bird) {
+        this.bird = bird;
+    }
+
+    @Override
+    public void squeak() {
+        bird.makeSound();
+    }
+}
